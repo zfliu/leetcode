161bd4a741b7206f9542713174c37cba9f93ffe2
liuzhuofeng <liuzhuofeng@yfcloud.com>
2020-03-10 12:10:45 +0800

    0004

--- 0004_median_of_two_sorted_arrays.cpp
+++ 0004_median_of_two_sorted_arrays.cpp
@@ -7,21 +7,24 @@ using namespace std;
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int mid1 = (nums1.size() + nums2.size() + 1) / 2;   // top mid1
-        int mid2 = (nums2.size() + nums2.size() + 2) / 2;   // top mid2
+        int mid1 = (nums1.size() + nums2.size() + 1) / 2;
+        int mid2 = (nums1.size() + nums2.size() + 2) / 2;
 
-        int total = getKth(nums1, 0, nums1.size() -1, nums2, 0 , nums2.size() -1, mid1)
-            + getKth(nums1, 0, nums1.size() -1, nums2, 0 , nums2.size() -1, mid2);
-        return total * 1.0 / 2; 
+        int k1 = getKth(nums1, 0, nums1.size(), nums2, 0 , nums2.size(), mid1);
+        int k2 = getKth(nums1, 0, nums1.size(), nums2, 0 , nums2.size(), mid2);
+        return (k1 + k2) * 1.0 / 2; 
     }
 
     int getKth(vector<int> &nums1, int begin1, int end1, vector<int> &nums2, int begin2, int end2, int k)
     {
-        if (nums2.size() > nums1.size()){
+        int len1 = end1 - begin1;
+        int len2 = end2 - begin2;
+
+        if (len2 > len1){
             // 保证入参的nums1一定是比较长的那一个
-            getKth(nums2, begin2, end2, nums1, begin1, end1, k);
+            return getKth(nums2, begin2, end2, nums1, begin1, end1, k);
         }
-        if (end2 - begin2 <= 0){
+        if (len2 <= 0){
             // 剩下一个数组,那么topK自然在剩下的数组里面选
             return nums1[begin1 + k - 1];
         }
@@ -30,22 +33,26 @@ public:
             return std::min(nums1[begin1], nums2[begin2]);
         }
 
-        int len1 = end1 - begin1;
-        int len2 = end2 - begin2;
 
-        int i = begin1 + std::min(len1, k / 2 + 1) - 1;
-        int j = begin2 + std::min(len2, k / 2 + 1) - 1;
+        int i1 = begin1 + std::min(len1, k / 2) - 1; // 要比较的数的坐标
+        int i2 = begin2 + std::min(len2, k / 2) - 1;
 
-        if (nums1[i] < nums2[j]){
-
+        if (nums1[i1] < nums2[i2]){
+            return getKth(nums1, i1 + 1, end1, nums2, begin2, end2, k - (i1 - begin1 + 1));
         }else{
-
+            return getKth(nums1, begin1, end1, nums2, i2 + 1, end2, k - (i2 - begin2 + 1));
         }
-
     }
 };
 
+void test_cast(vector<int> v1, vector<int> v2)
+{
+    Solution solu;
+    cout<<solu.findMedianSortedArrays(v1, v2)<<endl;
+}
+
 int main()
 {
-
+    test_cast({1,2}, {3,4});
+    test_cast({1}, {2, 3});
 }
