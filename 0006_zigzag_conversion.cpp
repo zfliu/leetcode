016631e16008e7a93c9630778b7b1145bd2bf674
liuzhuofeng <liuzhuofeng@yfcloud.com>
2020-04-10 12:24:21 +0800

    pre-create files

---
+++ 0006_zigzag_conversion.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+using namespace std;
+
+/*
+ * @lc app=leetcode.cn id=6 lang=cpp
+ *
+ * [6] Z 字形变换
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        return "";
+    }
+};
+// @lc code=end
+
+int main()
+{
+
+}
---
+++ 0008_string_to_Integer.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+using namespace std;
+
+/*
+ * @lc app=leetcode.cn id=8 lang=cpp
+ *
+ * [8] 字符串转换整数 (atoi)
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int myAtoi(string str) {
+        
+    }
+};
+// @lc code=end
+
+int main()
+{
+
+}
