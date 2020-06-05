019a536a3958be17ef9007a3ece686c243f3cce4
liuzhuofeng <liuzhuofeng@yfcloud.com>
2020-06-05 17:47:18 +0800

    111

--- 0007_reverse_integer.cpp
+++ 0007_reverse_integer.cpp
@@ -3,30 +3,48 @@
 #include <vector>
 using namespace std;
 
+/*
+ * @lc app=leetcode.cn id=7 lang=cpp
+ *
+ * [7] 整数反转
+ */
+
+// @lc code=start
 class Solution {
 public:
     int reverse(int x) {
-        if (x == 0x80000000){
-            return 0;
-        }
-        
-        int s = -1;
-        if (x < 0){
-            x = -x;
-            s = 1;
-        }
-
-        int r = 0;
-        while (x){
-            int d = x % 10;
+        // 1. 如果temp = rev * 10 + pop 导致溢出,那么一定又 rev >= INTMAX / 10
+        // 2. 如果 rev > INTMAX / 10, 那么temp = rev * 10 + pop 一定会溢出
+        // 3. 如果 rev == INTMAX / 10, 那么只要 pop > 7, temp = rev * 10 + pop 就会溢出
+        int INT_MAX = 0x7fffffff, INT_MIN = 0x80000000;
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
             x /= 10;
-            r *= 10;
-            r += d;
+            if (rev > INT_MAX/10 || (rev == INT_MAX / 10 && pop > 7)) 
+                return 0;
+            if (rev < INT_MIN/10 || (rev == INT_MIN / 10 && pop < -8)) 
+                return 0;
+            rev = rev * 10 + pop;
         }
-
-        return r * s;
+        return rev;
+    }
+    int reverse2(int x) {
+        // 如果y = y * 10 + x % 10溢出，则 y>=214748364
+        // 当y=214748364时，输入的值只能为：1463847412，此时不溢出
+        // 即：y > 214748364 || y < -214748364 必定溢出
+        int y = 0;
+        while (x != 0) {
+            if (y > 214748364 || y < -214748364) {
+                return 0;
+            }
+            y = y * 10 + x % 10;
+            x = x / 10;
+        }
+        return y;
     }
 };
+// @lc code=end
 
 int main()
 {
--- 0008_string_to_Integer.cpp
+++ 0008_string_to_Integer.cpp
@@ -14,7 +14,30 @@ using namespace std;
 class Solution {
 public:
     int myAtoi(string str) {
+        int result = 0;
         
+        for (int i = 0 ; i < str.size() ; i++){
+            
+            if (!isNumber(str[i])){
+                break;
+            }
+            if (isOverflow(1,2)){
+                return result > 0 ? 
+            }
+            
+        }
+        return result;
+    }
+    bool isOverflow(int32_t x, int32_t y){
+        int32_t z = x + y;
+        if(x > 0 && y > 0 && z < 0)
+            return true;  
+        if(x < 0 && y < 0 && z > 0)
+            return true;
+        return false;
+    }
+    bool isNumber(char ch){
+        return ch >= '0' && ch <= '9';
     }
 };
 // @lc code=end
