6c8113c6d115591b78eb79a6e7f6267721e0a7b9
zfliu <zfliu_6@163.com>
2020-07-21 22:18:29 +0800

    创建题目

---
+++ 0031_next_permutation.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=31 lang=cpp
+ *
+ * [31] 下一个排列
+ *
+ * https://leetcode-cn.com/problems/next-permutation/description/
+ *
+ * algorithms
+ * Medium (34.15%)
+ * Likes:    570
+ * Dislikes: 0
+ * Total Accepted:    73.8K
+ * Total Submissions: 216K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+ * 
+ * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+ * 
+ * 必须原地修改，只允许使用额外常数空间。
+ * 
+ * 以下是一些例子，输入位于左侧列，其相应输出位于右侧列。
+ * 1,2,3 → 1,3,2
+ * 3,2,1 → 1,2,3
+ * 1,1,5 → 1,5,1
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        
+    }
+};
+// @lc code=end
+
---
+++ 0047_permutations_ii.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=47 lang=cpp
+ *
+ * [47] 全排列 II
+ *
+ * https://leetcode-cn.com/problems/permutations-ii/description/
+ *
+ * algorithms
+ * Medium (59.36%)
+ * Likes:    348
+ * Dislikes: 0
+ * Total Accepted:    71K
+ * Total Submissions: 119.5K
+ * Testcase Example:  '[1,1,2]'
+ *
+ * 给定一个可包含重复数字的序列，返回所有不重复的全排列。
+ * 
+ * 示例:
+ * 
+ * 输入: [1,1,2]
+ * 输出:
+ * [
+ * ⁠ [1,1,2],
+ * ⁠ [1,2,1],
+ * ⁠ [2,1,1]
+ * ]
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        
+    }
+};
+// @lc code=end
+
